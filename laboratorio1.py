6440a18a602f742d4fb89f5b759117409d06e2e5
xballes <ballesxabi@gmail.com>
2022-10-05 09:18:40 +0200

    Meny y reemplazar ya funciona

--- laboratorio1.py
+++ laboratorio1.py
@@ -22,6 +22,52 @@ def labo1():
             
         #Intercambiar letras
         textoDescifrado = texto.maketrans(Frecuencia_texto, Frecuencia)
-        print(texto.translate(textoDescifrado))
+        mensaje=texto.translate(textoDescifrado)
+        #print(mensaje)
         
-labo1()         
+        def accion1():
+            print('Introduce la letra del texto que deseas modificar:')
+            letra=input()
+            print('Introduce la letra con la que la quieras sustituir:')
+            sustituta=input()
+            #print(mensaje)
+            modificacion=mensaje.replace(letra,sustituta)
+            print(modificacion)
+            
+
+        def salir():
+            print('Has salido')
+            
+        opciones = {
+        '1': ('Modificar texto', accion1),
+        '2': ('Salir', salir)
+        }
+        
+        def mostrar_menu(opciones):
+            print('Seleccione una opción:')
+        for clave in sorted(opciones):
+            print(f' {clave}) {opciones[clave][0]}')
+        
+        def leer_opcion(opciones):
+            while (a := input('Opción: ')) not in opciones:
+             print('Opción incorrecta, vuelva a intentarlo.')
+            return a
+        def ejecutar_opcion(opcion, opciones):
+            opciones[opcion][1]()
+        
+        def generar_menu(opciones, opcion_salida):
+            opcion = None
+            while opcion != opcion_salida:
+                mostrar_menu(opciones)
+                opcion = leer_opcion(opciones)
+                ejecutar_opcion(opcion, opciones)
+                print() # se imprime una línea en blanco para clarificar la salida por pantalla
+                
+        generar_menu(opciones, '2')
+       
+            
+
+    
+    
+
+labo1()    
